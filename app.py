a6ffff6eafcde315f9fa3ee960fc27ea30cf2f9f
AvazbekAvliyoqulov <96057659+AvazbekAvliyoqulov@users.noreply.github.com>
2022-01-21 14:36:44 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from googletrans import Translator
+import requests as re
+
+tr = Translator()
+
+def translate(text, lang):
+	transl = tr.translate(text, dest=lang)
+	trans = transl.text
+	return trans, lang
+
+text = input("Tarjima qilmoqchi bo'lgan matningizni kiriting: ")
+lang = input("Tarjima qilmoqchi bo'lgan til kodini kiriting: ")
+if True:
+	try:
+		if True:
+			trmatn = translate(text, lang)[0]
+			trlang = translate(text, lang)[1]
+			print("Siz kiritgan so'z:", text)
+			print("Tarjima qilingan matn:", trmatn)
+			print("Tarjima tili:", trlang)
+			text = input("Tarjima qilmoqchi bo'lgan matningizni kiriting: ")
+			lang = input("Tarjima qilmoqchi bo'lgan til kodini kiriting: ")
+	except Exception as e:
+		print("Xato, qayta urining!")
+		text = input("Tarjima qilmoqchi bo'lgan matningizni kiriting: ")
+		lang = input("Tarjima qilmoqchi bo'lgan til kodini kiriting: ")
